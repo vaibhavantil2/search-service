2f5b06b2d2df7a0cc427b15cfec5acc335e68c7d
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2021-08-04 17:51:16 +0530

    Update proccess_payment.py

--- ecom/payment_gateway/proccess_payment.py
+++ ecom/payment_gateway/proccess_payment.py
@@ -5,3 +5,8 @@ def proccess_payment_simulation(payment_method, card_hash):
     time.sleep(60)
     hashids = Hashids(alphabet='abcdefghijklmnopqrstuvwxyz1234567890', min_length=22)
     return hashids.encode(1)
+
+def proccess_return_products(payment_method, prod_details):
+    time.sleep(60)
+    hashids = Hashids(alphabet=payment_method.id)
+    return hashids.encode(1), prod_details.prod_id
